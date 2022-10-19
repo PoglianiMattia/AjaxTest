265b6919a1aee28c859920dc1dac3b9d30a4c0ff
MattiaPoglianiZBook <mattia.pogliani@gmail.com>
2022-10-19 17:25:08 +0200

    add map (incomplete)

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,32 +1,12 @@
 <template>
-  <q-page class="q-pa-md">
-      <q-card class="my-card">
-        <q-parallax src="https://picsum.photos/1000/1000" :height="200" />
-
-        <q-card-section>
-          <div class="text-h6">Contattaci</div>
-          <div class="text-subtitle2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
-            dolorem commodi beatae nulla tempore explicabo nostrum perferendis
-            ab in, ratione assumenda totam dicta ipsum eveniet a magnam veniam
-            temporibus sequi.
-          </div>
-        </q-card-section>
-      </q-card>
+  <q-page>
+    <div style="height: calc(100vh - 50px)">
+      <!-- 🤿 Vue, please render the Google Map Component here -->
+      <GMapMap :center="center" :zoom="10" />
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "ContactsPage",
-});
+<script setup>
+const center = { lat: 51.093048, lng: 6.84212 };
 </script>
-
-<style lang="scss">
-.my-card {
-  width: 100%;
-  max-width: 1000px;
-}
-</style>
