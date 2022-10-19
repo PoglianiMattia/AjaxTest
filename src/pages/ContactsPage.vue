6fc670ad303ad0f001f3c38b39c9a8b567f0b0f8
MattiaPoglianiZBook <mattia.pogliani@gmail.com>
2022-10-19 17:55:50 +0200

    fix map rendering

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,12 +1,51 @@
 <template>
   <q-page>
-    <div style="height: calc(100vh - 50px)">
-      <!-- 🤿 Vue, please render the Google Map Component here -->
-      <GMapMap :center="center" :zoom="10" />
-    </div>
+    <GMapMap
+      :center="center"
+      :zoom="12"
+      map-type-id="terrain"
+      style="height: calc(100vh - 50px)"
+    >
+      <GMapCluster :zoomOnClick="true">
+        <GMapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center = m.position"
+        />
+      </GMapCluster>
+    </GMapMap>
   </q-page>
 </template>
 
 <script setup>
-const center = { lat: 51.093048, lng: 6.84212 };
+const center = { lat: 45.274068, lng: 9.09342 };
+const markers = [
+  {
+    position: {
+      lat: 45.49972,
+      lng: 9.208701,
+    },
+  },
+  {
+    position: {
+      lat: 45.487205,
+      lng: 9.14965,
+    },
+  },
+  {
+    position: {
+      lat: 45.470595,
+      lng: 9.156345,
+    },
+  },
+  {
+    position: {
+      lat: 45.407962,
+      lng: 9.171622,
+    },
+  },
+];
 </script>
